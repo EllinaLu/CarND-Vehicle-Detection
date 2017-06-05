1c5288d1af8f150550f85f98d399e26a681a5eab
Ellina <ellina.lu@gmail.com>
2017-06-05 23:52:16 +0800

    Starting to write report

--- images.py
+++ images.py
@@ -139,46 +139,6 @@ def extract_features(imgs, color_space='BGR', spatial_size=(32, 32),
     # Return list of feature vectors
     return features
 
-# Define a function to extract features from a list of images
-# Have this function call bin_spatial() and color_hist()
-def extract_hog_features(imgs, cspace='BGR', orient=9, 
-                        pix_per_cell=8, cell_per_block=2, hog_channel=0):
-    # Create a list to append feature vectors to
-    features = []
-    # Iterate through the list of images
-    for file in imgs:
-        # Read in each one by one
-        #image = mpimg.imread(file)
-        image = cv2.imread(file)
-        # apply color conversion if other than 'RGB'
-        if cspace != 'BGR':
-            if cspace == 'HSV':
-                feature_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-            elif cspace == 'LUV':
-                feature_image = cv2.cvtColor(image, cv2.COLOR_BGR2LUV)
-            elif cspace == 'HLS':
-                feature_image = cv2.cvtColor(image, cv2.COLOR_BGR2HLS)
-            elif cspace == 'YUV':
-                feature_image = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
-            elif cspace == 'YCrCb':
-                feature_image = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
-        else: feature_image = np.copy(image)      
-
-        # Call get_hog_features() with vis=False, feature_vec=True
-        if hog_channel == 'ALL':
-            hog_features = []
-            for channel in range(feature_image.shape[2]):
-                hog_features.append(get_hog_features(feature_image[:,:,channel], 
-                                    orient, pix_per_cell, cell_per_block, 
-                                    vis=False, feature_vec=True))
-            hog_features = np.ravel(hog_features)        
-        else:
-            hog_features = get_hog_features(feature_image[:,:,hog_channel], orient, 
-                        pix_per_cell, cell_per_block, vis=False, feature_vec=True)
-        # Append the new feature vector to the features list
-        features.append(hog_features)
-    # Return list of feature vectors
-    return features
 
 # Define a function to extract features from a single image window
 # This function is very similar to extract_features()
@@ -344,12 +304,6 @@ def train_model():
                             cell_per_block=cell_per_block, 
                             hog_channel=hog_use_channel, spatial_feat=spatial_feat, 
                             hist_feat=hist_feat, hog_feat=hog_feat)
-    #car_features = extract_hog_features(cars, cspace=colorspace, orient=orient_bins, 
-    #                        pix_per_cell=pix_per_cell, cell_per_block=cell_per_block, 
-    #                        hog_channel=hog_use_channel)
-    #notcar_features = extract_hog_features(notcars, cspace=colorspace, orient=orient_bins, 
-    #                        pix_per_cell=pix_per_cell, cell_per_block=cell_per_block, 
-    #                        hog_channel=hog_use_channel)
 
                         
     # Create an array stack of feature vectors
